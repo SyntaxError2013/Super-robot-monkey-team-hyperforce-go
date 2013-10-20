232fe01a14315b18e6c680d9187f92744829cd97
rushil <rushil9292@gmail.com>
2013-10-20 15:12:04 +0530

    Elementary scoring system

--- handlers/home.php
+++ handlers/home.php
@@ -10,13 +10,38 @@ class HomeHandler{
 		}
 	function post_xhr(){
 		$source = $_POST['source'];
-		$keystring = $_POST['keystring'];
+		$keystring = urlencode($_POST['keystring']);
 		$details = getDetailsBySource($source,$keystring);
-		echo json_encode($details);
+		//echo json_encode($details);
 		foreach ($details as $key => $article) {
 			$scores[$key] = apiCall($article["comments"], $keystring);
 		}
-		echo json_encode($scores);
+		//echo(json_encode($scores));
+		
+		$i = 0;
+		$total = 0;
+		$positive = 0;
+		$negative = 0;
+		foreach($scores as $key=>$article_score){
+			$article_score = json_decode($article_score,1);
+			foreach($article_score['results'] as $comment){
+				$total += $comment['score'];
+				if($comment['score']){
+					$i+=1;
+				}
+				if($comment['score']> 0 ){
+					$positive+=1;
+				}
+				if($comment['score']<0 ){
+					$negative+=1;
+				}
+			}
+		}
+		echo 10*$total/$i + 5;
+		echo "\n";
+		echo $positive;
+		echo "\n";
+		echo $negative;
 
 	}
 }
