b12e74eb87a85412373de0a30886cc9514b2e645
jayant1992 <jayant_jain1992@yahoo.co.in>
2013-10-20 14:48:19 +0530

    Updated repustate api key

--- config/settings.php
+++ config/settings.php
@@ -12,6 +12,7 @@ $hinduSearchID = "010961665768017960356:trzulzp-s_4";
 
 $customSearchUrl = "https://www.googleapis.com/customsearch/v1?key=$apiKey";
 
-$repustateAPIKey = "d96df10c7b6a0d5846f7c33c427085f7558bf003";
+// $repustateAPIKey = "d96df10c7b6a0d5846f7c33c427085f7558bf003";
+$repustateAPIKey = "68306bc8217d78bad1520ba2c19082086d2781d2";
 $repustateEndPoint = "http://api.repustate.com/v2/$repustateAPIKey/bulk-score.json";
 ?>
